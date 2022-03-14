9378df71f589165db927b0a702906d5e23d4cbfa
Peter-Pater <xh721@nyu.edu>
2022-03-13 18:19:58 -0700

    drawing pad

---
+++ Web-RTC/Assets/Scripts/Paintable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+    public GameObject Brush;
+    public Camera drawingCam;
+    public float BrushSize = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = drawingCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                GameObject newBrush = Instantiate(Brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+                newBrush.transform.localScale = Vector3.one * BrushSize;
+            }
+        }
+    }
+}
