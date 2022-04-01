98e1b0d749e04c8b6ee35071cf6a7fab3577d63d
Peter-Pater <xh721@nyu.edu>
2022-04-01 14:58:33 -0700

    add video audio to shared surface

---
+++ RemoteController/Assets/Scripts/ConnectionControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.WebRTC.Unity;
+
+public class ConnectionControl : MonoBehaviour
+{
+    public PeerConnection myPeerConnection;
+    public NodeDssSignaler myNodeDssSignaler;
+
+    private string localPeerId;
+    private string remotePeerId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.localPeerId = this.myNodeDssSignaler.LocalPeerId;
+        this.remotePeerId = this.myNodeDssSignaler.RemotePeerId;
+    }
+
+    public void StartConnection()
+    {
+        if (!string.IsNullOrEmpty(localPeerId) && !string.IsNullOrEmpty(remotePeerId))
+        {
+            myPeerConnection.StartConnection();
+        }
+        else
+        {
+            Debug.Log("[START CONNECTION] peer ids not provided!");
+        }
+    }
+}
