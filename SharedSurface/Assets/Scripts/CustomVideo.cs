2ca56b7b537f2c0f2cc2d4ab02b96e0a06bf070a
Peter-Pater <xh721@nyu.edu>
2022-05-18 18:45:32 -0700

    start the hard dev

---
+++ SharedSurface/Assets/Scripts/CustomVideo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR;
+
+namespace Microsoft.MixedReality.WebRTC.Unity
+{
+    public class CustomVideo : CustomVideoSource<Argb32VideoFrameStorage>
+    {
+        protected override void OnFrameRequested(in FrameRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
