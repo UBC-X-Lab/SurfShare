a8eb607ec1126e8bfe778261a4e3e5502e8f25ed
Peter-Pater <xh721@nyu.edu>
2022-06-06 09:31:32 -0700

    conversion results 1

--- SharedSurface/Assets/Scripts/CoordinateSystemHelper.cs
+++ SharedSurface/Assets/Scripts/CoordinateSystemHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using Microsoft.Windows.Perception.Spatial;
 using Microsoft.MixedReality.Toolkit.WindowsMixedReality;
 using UnityEngine.XR.WindowsMR;
 
@@ -12,11 +13,12 @@ namespace CameraFrameUtilities
 {
     public static class CoordinateSystemHelper
     {
-        #if UNITY_WSA && !UNITY_EDITOR
+#if UNITY_WSA && !UNITY_EDITOR
             // from unity coordinates to frame coordinate. Note the taken argument is WinRT's definition for SpatialCoordinateSystem
             public static System.Numerics.Vector3? GetFramePosition(SpatialCoordinateSystem frameCoordinateSystem, Vector3 unityPosition)
             {
                 // convert Unity Coordinates to Hololens Coordinates (left-handed to right handed)
+                Debug.Log("Unity Position - " + "X:" + unityPosition.x + ", Y:" + unityPosition.y + ", Z:" + unityPosition.z);
                 System.Numerics.Vector3 HLCoordinate = NumericsConversionExtensions.ToSystem(unityPosition); // Is the hololens world origin the same as unity origin? For now we don't need rotation
                 System.Numerics.Matrix4x4? transformToFrame = WindowsMixedRealityUtilities.SpatialCoordinateSystem.TryGetTransformTo(frameCoordinateSystem);
                 if (transformToFrame.HasValue)
@@ -31,7 +33,7 @@ namespace CameraFrameUtilities
                     return null;
                 }
             }
-        #endif
+#endif
     }
 }
 
