a0e9a744f8d796136f88c7cacb429833b87a39fe
Peter-Pater <xh721@nyu.edu>
2022-06-30 01:06:50 -0700

    opencv mog2 optimized history

--- background_subtraction/opencv_test.py
+++ background_subtraction/opencv_test.py
@@ -1,17 +1,18 @@
 #from __future__ import print_function
 import cv2 as cv
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
                                               OpenCV. You can process both videos and images.')
-parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='vtest.avi')
+parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='test_images/real_test1.mp4')
 parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
 args = parser.parse_args()
 
 ## [create]
 #create Background Subtractor objects
 if args.algo == 'MOG2':
-    backSub = cv.createBackgroundSubtractorMOG2(detectShadows=False, history=0)
+    backSub = cv.createBackgroundSubtractorMOG2(detectShadows=False, history=1000)
 elif args.algo == 'KNN':
     backSub = cv.createBackgroundSubtractorKNN()
 elif args.algo == "GMG":
@@ -32,6 +33,9 @@ if not capture.isOpened():
 
 count = 0
 
+frame_array = []
+size = (1280, 360)
+
 while True:
     ret, frame = capture.read()
     if frame is None:
@@ -49,15 +53,20 @@ while True:
                cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
     ## [display_frame_number]
 
-    ## [show]
-    #show the current frame and the fg masks
-    # cv.imshow('Frame', frame)
-    # cv.imshow('FG Mask', fgMask)
-    cv.imwrite(f"result/img-{count}.png", fgMask)
+    # cv.imwrite(f"result/img-{count}.png", fgMask)
+    combined_frame = np.stack((fgMask,)*3, axis=-1)
+    combined_frame = np.concatenate((frame, combined_frame), axis=1)
+    frame_array.append(combined_frame)
     ## [show]
 
     count += 1
 
     keyboard = cv.waitKey(30)
+    # if keyboard == 'q' or keyboard == 27 or count == 60:
     if keyboard == 'q' or keyboard == 27:
         break
+
+out = cv.VideoWriter('mog_mask1000.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, size) # last parameter set color to gray scale
+for i in range(len(frame_array)):
+    out.write(frame_array[i])
+out.release()
