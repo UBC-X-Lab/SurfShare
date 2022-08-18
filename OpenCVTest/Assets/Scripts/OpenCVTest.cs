01e4a167b4a11a482b1e4970ef8ac0397e13466d
Peter-Pater <xh721@nyu.edu>
2022-08-17 20:56:36 -0700

    mesh creation HL works

--- OpenCVTest/Assets/Scripts/OpenCVTest.cs
+++ OpenCVTest/Assets/Scripts/OpenCVTest.cs
@@ -2,29 +2,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-#if UNITY_WSA && !UNITY_EDITOR
 using OpenCvSharp;
-#endif
+using DelaunatorSharp.Unity.Extensions;
 
 public class OpenCVTest : MonoBehaviour
 {
-    Material myMaterial;
-    Texture2D originalTex;
-    Texture2D contouredTex;
+    static Material myMaterial;
+    static Texture2D originalTex;
+    static Texture2D contouredTex;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
         myMaterial = this.GetComponent<Renderer>().material;
-        originalTex = (Texture2D) myMaterial.mainTexture;
+        originalTex = (Texture2D)myMaterial.mainTexture;
+        test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void test()
+    {
+
         byte[] imageData = originalTex.GetRawTextureData();
-#if UNITY_WSA && !UNITY_EDITOR
+
+        //Debug.Log(1);
         Mat image = new Mat(originalTex.height, originalTex.width, MatType.CV_8UC3, imageData);
-        Mat greyImage = image.CvtColor(ColorConversionCodes.BGR2GRAY).Threshold(127, 255, ThresholdTypes.Binary);
-
+        //Debug.Log(2);
+        Mat greyImage = image.CvtColor(ColorConversionCodes.BGR2GRAY).Threshold(127, 255, ThresholdTypes.Binary); // pay attention to the import setting for the texture
+        //Debug.Log(3);
         Point[][] contours = greyImage.FindContoursAsArray(RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
-
+        //Debug.Log(4);
         // filter contours that are too small
         List<Point[]> res_con = new List<Point[]>();
         for (int i = 0; i < contours.Length; i++)
@@ -34,25 +49,42 @@ public class OpenCVTest : MonoBehaviour
                 // contour approx
                 double eps = 0.01 * Cv2.ArcLength(contours[i], true);
                 res_con.Add(Cv2.ApproxPolyDP(contours[i], eps, true));
-                Debug.Log(res_con[res_con.Count - 1].Length);
-                Debug.Log(Cv2.ContourArea(contours[i]));
+                //Debug.Log(res_con[res_con.Count - 1].Length);
+                //Debug.Log(Cv2.ContourArea(contours[i]));
             }
         }
-        
-        Cv2.DrawContours(image, res_con, -1, new Scalar(0, 255, 0), 3);
+
+        Cv2.DrawContours(image, res_con, -1, new Scalar(0, 255, 0), 3); // the contours points are ordered clock-wise
+        //foreach (Point point in res_con[0])
+        //{
+        //    Debug.Log(point.X + "," + point.Y);
+        //}
+
         //byte[] resImageData = new byte[imageData.Length];
 
         //image.GetArray(out resImageData);
         contouredTex = new Texture2D(originalTex.width, originalTex.height, originalTex.format, mipChain: false);
         contouredTex.LoadRawTextureData(image.Data, imageData.Length);
-#endif
+
         myMaterial.mainTexture = contouredTex;
         contouredTex.Apply();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float image_width = transform.localScale.x;
+        float image_height = transform.localScale.y;
+        Vector3 image_origin = transform.position - new Vector3(image_width / 2, 0, image_height / 2);
+
+        //create mesh from them
+        foreach (Point[] con in res_con)
+        {
+            Vector2[] vertices = new Vector2[con.Length];
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i] = new Vector2(con[i].X / (float) originalTex.width * image_width, con[i].Y / (float) originalTex.height * image_height);
+            }
+            GameObject obj = Instantiate(GameObject.Find("Object"));
+            obj.transform.position = image_origin;
+            obj.GetComponent<UpdateMesh>().enabled = true;
+            obj.GetComponent<MeshFilter>().mesh = MeshCreator.CreateMesh(vertices);
+        }
     }
 }
---
+++ OpenCVTest/Assets/Scripts/UpdateMesh.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMesh : MonoBehaviour
+{
+    public float height = 0.5f;
+    public bool skew = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (height != 0)
+        {
+            MeshCreator.UpdateMeshHeight(GetComponent<MeshFilter>().mesh, height);
+        }
+
+        if (skew)
+        {
+            MeshCreator.SkewMesh(GetComponent<MeshFilter>().mesh);
+        }
+    }
+}
