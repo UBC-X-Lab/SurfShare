ab9eb12d8fdec5a882000701c9eae297c8b52c77
Peter-Pater <xh721@nyu.edu>
2022-08-23 21:58:42 -0700

    desktop mesh sync completed

---
+++ OpenCVTest_clone_0/Assets/Scripts/DelaunatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelaunatorTest : MonoBehaviour
+{
+    public GameObject baseObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2[] poly_vertices = new Vector2[4];
+        poly_vertices[0] = new Vector2(0, 0);
+        poly_vertices[1] = new Vector2(0, 1);
+        poly_vertices[2] = new Vector2(1, 1);
+        poly_vertices[3] = new Vector2(1, 0);
+
+        GameObject newObject = Instantiate(baseObject);
+        Mesh newMesh = MeshCreator.CreateMesh(poly_vertices);
+        newObject.GetComponent<MeshFilter>().mesh = newMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
